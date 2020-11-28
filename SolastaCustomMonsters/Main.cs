09f5cc43d419ec464aa7bd7642ea29ea47761c53
Paulo Monteiro <pauloesquilo@gmail.com>
2020-11-28 12:25:41 -0800

    Add project files.

---
+++ SolastaCustomMonsters/Main.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Reflection;
+using UnityModManagerNet;
+using HarmonyLib;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace SolastaCustomMonsters
+{
+    public class Main
+    {
+        // [System.Diagnostics.Conditional("DEBUG")]
+        public static void Log(string msg)
+        {
+            if (logger != null) logger.Log(msg);
+        }
+
+        public static void Error(Exception ex)
+        {
+            if (logger != null) logger.Error(ex.ToString());
+        }
+
+        public static void Error(string msg)
+        {
+            if (logger != null) logger.Error(msg);
+        }
+
+        public static UnityModManager.ModEntry.ModLogger logger;
+        public static bool enabled;
+
+        static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            try
+            {
+                logger = modEntry.Logger;
+                var harmony = new Harmony(modEntry.Info.Id);
+                harmony.PatchAll(Assembly.GetExecutingAssembly());
+            }
+            catch (Exception ex)
+            {
+                Error(ex);
+                throw;
+            }
+            return true;
+        }
+
+        [HarmonyPatch(typeof(MainMenuScreen), "RuntimeLoaded")]
+        static class MainMenuScreen_RuntimeLoaded_Patch
+        {
+            static void Postfix()
+            {
+                JObject config = JObject.Parse(File.ReadAllText(UnityModManager.modsPath + @"/SolastaCustomMonsters/Monsters.json"));
+
+                try
+                {
+                    JObject packages = (JObject)config.GetValue("packages");
+                    ParsePackages(packages);
+
+                }
+                catch
+                {
+                    Log("items entry not found");
+                }
+                try
+                {
+                    JObject monsters = (JObject)config.GetValue("monsters");
+                    ParseMonsters(monsters);
+
+                }
+                catch {
+                    Log("monsters entry not found");
+                }
+            }
+
+            static void ParsePackages(JObject monsters)
+            {
+                Log("parse items not implemented yet");
+            }
+
+            static void ParseMonsters(JObject monsters)
+            {
+                foreach (var monster in monsters)
+                {
+                    MonsterDefinition monsterDefinition;
+                    try
+                    {
+                        monsterDefinition = DatabaseRepository.GetDatabase<MonsterDefinition>().GetElement(monster.Key);
+                        Log("Found monster: " + monster.Key);
+                    }
+                    catch
+                    {
+                        Log("Monster not found: " + monster.Key);
+                        continue;
+                    }
+                    foreach (var item in (JObject) monster.Value)
+                    {
+                        // should do something here...
+                    }
+
+                    }
+                }
+            }
+        }
+    }
---
+++ SolastaCustomMonsters/README.md
@@ -0,0 +1,64 @@
+# Solasta Custom Monsters
+
+This Mod allows you to customize any vendor stock in Solasta: Crown of the Magister
+
+# How to Compile
+
+1. Install Visual Studio 2019
+2. Edit SolastaCustomMonsters.csproj and fix your game folder on line 5
+3. Use "InstallDebug" to have it installed directly to your Mods folder
+
+# How to Install
+
+1. Download and instal [Unity Mod Manager](https://www.nexusmods.com/site/mods/21)
+2. Add below *GameInfo* XML tag, under *Config* tag, to $UNITY_MOD_MANAGER_HOME\UnityModManagerConfig.xml:
+```
+	<GameInfo Name="Solasta">
+		<Folder>Solasta</Folder>
+		<ModsDirectory>Mods</ModsDirectory>
+		<ModInfo>Info.json</ModInfo>
+		<GameExe>Solasta.exe</GameExe>
+		<EntryPoint>[UnityEngine.UIModule.dll]UnityEngine.Canvas.cctor:Before</EntryPoint>
+		<StartingPoint>[Assembly-CSharp.dll]RuntimeManager.LoadRuntime:After</StartingPoint>
+		<MinimalManagerVersion>0.22.13</MinimalManagerVersion>
+	</GameInfo>
+```
+3. Execute Unity Mod Manager, Select Solasta, and Install
+4. Select Mods tab, drag and drop from releases
+
+# How to Debug
+
+1. Open Solasta game folder
+	* Rename Solasta.exe to Solasta.exe.original
+	* Rename UnityPlayer.dll to UnityPlayer.dll.original
+	* Add below entries to *Solasta_Data\boot.config*:
+```
+wait-for-managed-debugger=1
+player-connection-debug=1
+```
+2. Download and install [7zip](https://www.7-zip.org/a/7z1900-x64.exe)
+3. Download [Unity Editor 2019.4.1](https://unity3d.com/get-unity/download/archive)
+4. Open Downloads folder
+	* Right-click UnitySetup64-2019.4.1f1.exe, 7Zip -> Extract Here
+	* Navigate to Editor\Data\PlaybackEngines\windowsstandalonesupport\Variations\win64_development_mono
+	* Copy *WindowsPlayer.exe* (and rename to *Solasta.exe*), *UnityPlayer.dll* and *WinPixEventRuntime.dll* to Solasta game folder
+
+You can now attach the Unity Debugger from Visual Studio 2019, Debug -> Attach Unity Debug
+
+# How to Customize
+
+1. Open $SOLASTA_HOME/Mods/SolastaCustomMonsters/Monsters.json on a text editor
+2. Add new monsters to the list
+3. Add new items under monsters
+4. (Optionally), set stock properties on items (i.e: faction, max amount, initial amount)
+
+You can lookup all existing monsters and items names at https://github.com/spacehamster/SolastaDataminer/releases/tag/0.3.3
+
+# Credits
+
+* @Spacehamster for all coding/hacking support (my other car is a cdr. answer is certainly 42!).
+* @susphiciousone for original idea.
+
+# TODO
+
+* Implement custom items creation
